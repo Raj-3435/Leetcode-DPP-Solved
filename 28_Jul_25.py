48b03f8f975770b09dc7bd8ebf0ac27fba6b6635
Aditya Raj <adityarajic1101@gmail.com>
2025-07-28 17:43:35 +0530

    Problem 2044

---
+++ 28_Jul_25.py
@@ -0,0 +1,9 @@
+# Problem 2044. Count Number of Maximum Bitwise-OR Subsets
+
+class Solution:
+    def countMaxOrSubsets(self, a: List[int]) -> int:
+        res = 0
+        for m in range(1, 1<<len(a)):
+            res += reduce(or_, (v for i,v in enumerate(a) if 1<<i&m)) == reduce(or_,a)
+
+        return res
